2a640a11c7391f35018a14394c44570f07131faf
unknown <ngocduc.2109@gmail.com>
2021-04-27 09:26:49 +0700

    [Thực hành] Validate email

---
+++ validateEmail/src/EmailExample.java
@@ -0,0 +1,17 @@
+import java.util.regex.*;
+
+public class EmailExample {
+    private static Pattern pattern;
+    private Matcher matcher;
+
+    private static final String EMAIL_REGEX = "^[A-Za-z0-9]+[A-Za-z0-9]*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)$";
+
+    public EmailExample(){
+        pattern = Pattern.compile(EMAIL_REGEX);
+    }
+
+    public boolean validate(String email){
+        matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+}
